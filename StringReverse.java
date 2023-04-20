7ae309a87a163d28a947ccf4595a802ada11adcd
sairam1555 <130015515+sairam1555@users.noreply.github.com>
2023-04-20 15:41:17 -0400

    files upload

---
+++ StringReverse.java
@@ -0,0 +1,15 @@
+package practice.copy;
+
+public class StringReverse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s="Sairam";
+		StringBuilder sb= new StringBuilder();
+		sb.append(s);
+		sb.reverse();
+		System.out.println(sb);
+
+	}
+
+}
